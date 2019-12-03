f6291b8288975e57a3cca0340023ff19041f44b9
mehulkumar18021994 <you@example.com>
2019-12-03 08:02:10 +0530

    word count finished

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,6 +1,7 @@
 
 from django.http import HttpResponse
-from django.shortcuts import render 
+from django.shortcuts import render
+import operator 
 
 def homepage(request):
     return render(request,'word-count-home.html')
@@ -16,4 +17,20 @@ def demoTemplate(request):
     return render(request,'home.html',{'key':'this value is passed in html page'})
 
 def countwords(request):
-    return render(request,'count.html')
+    fulltext = request.GET['fulltext']
+    #print(fulltext)
+    wordlist = fulltext.split()
+    
+    wordCountDict = {}
+
+    for word in wordlist:
+        if word in wordCountDict:
+            #increase 
+            wordCountDict[word] += 1
+        else:
+            #add to dict
+            wordCountDict[word] = 1
+
+        sortedwords = sorted(wordCountDict.items(),key=operator.itemgetter(1),reverse=True)
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'word_dict':sortedwords})
